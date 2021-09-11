fc3e7de6a736cd27d57eb0a14f0f46c7ce8b2323
Biswajit8637 <86891864+Biswajit8637@users.noreply.github.com>
2021-09-12 00:48:43 +0530

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3rdproject
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            char z = char.Parse(textBox3.Text);
+            switch (z) { 
+                case '+':
+                    textBox4.Text = (x + y).ToString();
+                    break;
+                case '-':
+                    textBox4.Text = (x - y).ToString();
+                    break;
+                case '*':
+                    textBox4.Text = (x * y).ToString();
+                    break;
+                case '/':
+                    textBox4.Text = (x / y).ToString();
+                    break;
+                case '%':
+                    textBox4.Text = (x % y).ToString();
+                    break;
+                default:
+                    textBox4.Text = "invalid AO";
+                    break;
+
+            }
+
+        }
+    }
+}
